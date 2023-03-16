597b6ec8cb7eb3ef5a34534ab978c6f92e6d6fcf
nyarla <nyarla@kalaclista.com>
2023-03-16 14:14:21 +0900

    feat: add lualine plugin

--- lua/kalaclista/plugins.lua
+++ lua/kalaclista/plugins.lua
@@ -3,7 +3,8 @@ require("lazy").setup(
     require("plugins.neo-tree"),
     require("plugins.treesitter"),
     require("plugins.cmp"),
-    require('plugins.neoformat'),
-    require("plugins.null-ls")
+    require("plugins.neoformat"),
+    require("plugins.null-ls"),
+    require("plugins.lualine")
   }
 )
