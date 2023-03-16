8426df596cf0c4bd252f3b6f03f1e27d6d716b62
nyarla <nyarla@kalaclista.com>
2023-03-16 14:51:43 +0900

    feat: add keymap.lua

--- init.lua
+++ init.lua
@@ -2,5 +2,6 @@ require("kalaclista.options")
 require("kalaclista.colorscheme")
 require("kalaclista.lazy")
 require("kalaclista.plugins")
+require("kalaclista.keymap")
 
 vim.cmd.colorscheme("kalaclista")
---
+++ lua/kalaclista/keymap.lua
@@ -0,0 +1,12 @@
+local function nnoremap(key, action)
+  vim.api.nvim_set_keymap("n", key, action, {noremap = true, silent = true})
+end
+
+local function vnoremap(key, action)
+  vim.api.nvim_set_keymap("v", key, action, {noremap = true, silent = true})
+end
+
+vnoremap("zs", ":sort<CR>")
+vnoremap("zS", ":sort!<CR>")
+
+nnoremap("<C-n>", ":Neotree toggle<CR>")
