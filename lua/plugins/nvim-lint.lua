da1b25f50f51ccb9a60adaaa263d5348ac8ceca7
nyarla <nyarla@kalaclista.com>
2024-03-12 11:57:53 +0900

    refactor(nvim-lint): load plugin by lib.plugins.mkPlugin helper

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,30 +1,47 @@
-local custom = {
-  deadnix = require("lib.linters.deadnix"),
-  textlint = require("lib.linters.textlint"),
-}
+local lib = require("lib.plugins")
 
-vim.api.nvim_create_autocmd({ "InsertEnter", "BufWritePost", "TextChanged" }, {
-  callback = function()
-    require("lint").try_lint()
+local M = lib.mkPlugin({
+  pname = "nvim-lint",
+  src = lib.fetchFromGitHub({
+    owner = "mfussenegger",
+    repo = "nvim-lint",
+    rev = "e824adb9bc01647f71e55457353a68f0f37f9931",
+  }),
+
+  activatePhase = function()
+    return {
+      lazy = true,
+      event = "InsertEnter",
+    }
+  end,
+
+  configurePhase = function()
+    vim.api.nvim_create_autocmd({ "InsertEnter", "BufWritePost", "TextChanged" }, {
+      callback = function()
+        require("lint").try_lint()
+      end,
+    })
+
+    return {
+      config = function()
+        local plugin = require("lint")
+
+        plugin.linters_by_ft = {
+          dockerfile = { "hadolint" },
+          markdown = { "textlint" },
+          nix = { "statix", "deadnix" },
+        }
+
+        for _, linters in pairs(plugin.linters_by_ft) do
+          for _, name in ipairs(linters) do
+            plugin.linters[name] = require("lib.linters." .. name)
+          end
+        end
+
+        return true
+      end,
+    }
   end,
 })
 
-return {
-  "mfussenegger/nvim-lint",
-  lazy = true,
-  event = "InsertEnter",
-  config = function()
-    local linters = require("lint")
-
-    linters.linters_by_ft = {
-      nix = { "statix", "deadnix" },
-      markdown = { "textlint" },
-    }
-
-    for key, val in pairs(custom) do
-      linters.linters[key] = val
-    end
-
-    return true
-  end,
-}
+return M
