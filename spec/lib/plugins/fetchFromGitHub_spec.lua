e045fa7c3439d5828491c41e4f9c5de7d2fa9f1d
nyarla <nyarla@kalaclista.com>
2024-03-17 10:23:25 +0900

    chore(plugins): remove pin flags from fetchFromGitHub

--- spec/lib/plugins/fetchFromGitHub_spec.lua
+++ spec/lib/plugins/fetchFromGitHub_spec.lua
@@ -10,7 +10,6 @@ describe("lib.plugins", function()
     assert.are.same(src, {
       url = "https://github.com/gpanders/editorconfig.nvim.git",
       commit = "5b9e303e1d6f7abfe616ce4cc8d3fffc554790bf",
-      pin = true,
     })
   end)
 end)
