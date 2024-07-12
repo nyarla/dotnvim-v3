a237ea7b1ac071030671eb7f87bcbb92988b7bc4
nyarla <nyarla@kalaclista.com>
2024-07-12 12:55:41 +0900

    refactor(kalaclista.plugins): make configurable to load plugin bundles

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 require("kalaclista.options").setup()
 require("kalaclista.colorscheme").setup()
 require("kalaclista.lazy").setup()
-require("kalaclista.plugins")
+require("kalaclista.plugins").setup({
+  use = { "library", "ui", "filetype", "editor", "completion" },
+})
 require("kalaclista.keymap")
